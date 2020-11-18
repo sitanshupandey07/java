4af507f793f52225adb10ad7189ca8cfde19e26c
sitanshupandey07 <70499917+sitanshupandey07@users.noreply.github.com>
2020-11-18 15:51:53 +0530

    prac8.java

---
+++ prac8.java
@@ -0,0 +1,10 @@
+public class argument {
+       public static void main(String [ ] args) 
+   {
+              int a= Integer. parseInt ( args[0]) ;
+              int b= Integer. parseInt ( args[1]) ;
+              int sum=a + b;
+
+       System. out. println("The Sum is " +sum) ;
+   }
+}
